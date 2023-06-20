8b9eed8068147a767dfa697353142595c5e58bfc
HarryOh1 <ohyh99@naver.com>
2023-06-20 10:56:35 +0900

    05

--- app/page.js
+++ app/page.js
@@ -1,41 +1,32 @@
-// async function fetchIpinfo() {
-//   const response = await fetch('https://api.ip.pe.kr/json')
-//   const ipinfo = await response.json()
-//   return ipinfo
-// }
-
-// export default async function Home() {
-//   const ipinfo = await fetchIpinfo()
-//   return (
-//     <>
-//       <h1>{ipinfo.ip}</h1>
-//     </>
-//   )
-// }
-
-'use client';
-import { useState, useEffect } from 'react';
+'use client'
+import { useState, useEffect } from 'react'
 
 function HomePage() {
-  const [ip, setIP] = useState('');
+  const [ip, setIP] = useState('')
+  const [countryName, setCountryName] = useState('')
+  const [countryCode, setCountryCode] = useState('')
 
   const getData = async () => {
-    const res = await fetch('https://api.ip.pe.kr/json/');
-    const data = await res.json();
-    console.log(data);
-    setIP(data.ip);
-  };
+    const res = await fetch('https://api.ip.pe.kr/json/')
+    const data = await res.json()
+    setIP(data.ip)
+    setCountryName(data.country_name.ko)
+    setCountryCode(data.country_code)
+  }
 
   useEffect(() => {
-    getData();
-  }, []);
+    getData()
+  }, [])
 
   return (
-    <div>
-      <h2>Your IP Address is</h2>
-      <h4>{ip}</h4>
-    </div>
-  );
+    <main className="font-['Nanum_Gothic'] text-center">
+      <p className='text-xl'>당신의 IP주소는</p>
+      <h1 className='font-bold text-4xl mt-2'>{ip}</h1>
+      <p className='text-lg mt-4'>
+        접속국가는 {countryName}({countryCode}) 입니다.
+      </p>
+    </main>
+  )
 }
 
-export default HomePage;
+export default HomePage
